2820c306f8e90dc971dbbd38eb25a43569ad4c7b
Rick Klaasboer <rick@klaasboer.org>
2023-10-30 23:50:06 +0100

    feat: make the page beautiful

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,9 +5,12 @@
  */
 
 import axios from 'axios';
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+require('bootstrap')
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
--- resources/views/link/create.blade.php
+++ resources/views/link/create.blade.php
@@ -1,15 +1,41 @@
-@if(session()->has('message'))
-    <p style="color: limegreen">{!! session()->get('message') !!}</p>
-@endif
+@extends('layout.app')
 
-<form method="POST" action="{{ route('link.store') }}">
-    @csrf
-    <label for="destination">Destinaton</label>
-    <input id="destination" type="text" name="destination">
+@section('title', 'Shorten a link')
 
-    @error('destination')
-    <div style="color: red">{{ $message }}</div>
-    @enderror
+@section('content')
 
-    <button type="submit">Create</button>
-</form>
+    <div class="container">
+        <div class="row justify-content-center align-items-center mt-5">
+            <div class="col-7">
+                <div class="card shadow-sm border-light-subtle">
+                    <div class="card-body p-4">
+
+                        @if(session()->has('message'))
+                            <div class="alert alert-success" role="alert">
+                                {!! session()->get('message') !!}
+                            </div>
+                        @endif
+
+                        <h3 class="fw-bolder">Shorten a long link</h3>
+                        <hr class="border border-light-subtle my-2">
+
+                        <form method="POST" action="{{ route('link.store') }}">
+                            @csrf
+                            <label for="destination">Destination</label>
+                            <input id="destination" value="{{ old('destination') }}" type="text" name="destination"
+                                   autocomplete="off"
+                                   class="form-control form-control-lg mb-2">
+
+                            @error('destination')
+                            <div class="text-danger">{{ $message }}</div>
+                            @enderror
+
+                            <button type="submit" class="btn btn-primary mt-2">Create your link!</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
